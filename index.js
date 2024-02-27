c7730c12906bbf827cc3336a69ab6155e1099db1
shahid23-dev <107908573+shahid23-dev@users.noreply.github.com>
2024-02-27 16:25:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello guys welcomr\n'
+  const msg = 'Hello guys welcomer\n'
   res.end(msg);
 });
 
